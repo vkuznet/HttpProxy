3048c79b063400f682ebda1ec0acc8903510d547
Valentin Kuznetsov <vkuznet@gmail.com>
2013-06-26 12:22:56 -0400

    Bug fix

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 pid=`ps | grep HttpProxy | grep -v grep | awk '{print $1}'`
 if  [ -n "$pid" ]; then
     echo "Kill previous HttpProxy, pid=$pid"
-    kill -2 $pid
+    kill -9 $pid
 fi
 nohup ./HttpProxy 2>&1 1>& http_proxy.log < /dev/null &
