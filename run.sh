424f8c393ff8af5c646ae6ee43bc9dcb20e90e01
Valentin Kuznetsov <vkuznet@gmail.com>
2013-06-26 14:49:14 -0400

    Printout new pid

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ if  [ -n "$pid" ]; then
     echo "Kill previous HttpProxy, pid=$pid"
     kill -9 $pid
 fi
-nohup ./HttpProxy 2>&1 1>& http_proxy.log < /dev/null &
+nohup ./HttpProxy -port=:9998 -verbose=1 2>&1 1>& http_proxy.log < /dev/null &
+npid=`ps | grep HttpProxy | grep -v grep | awk '{print $1}'`
+echo "Started HttpProxy, pid=$npid"
